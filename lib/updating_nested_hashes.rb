d87ff59b9e6be9021c5345e803467bd27401a5ec
anna bateman <annacbateman@gmail.com>
2020-04-08 17:42:38 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -61,7 +61,7 @@ def add_characters
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
 
-{:additional_characters[array]
+{:additional_characters[array]}
 
 
 
